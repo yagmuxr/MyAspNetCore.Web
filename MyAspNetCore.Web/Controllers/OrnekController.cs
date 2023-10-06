28d40e9985ff0eeec5b693607ccf0258a429c710
Yağmur Yalçın <yagmuryalcin26@hotmail.com>
2023-10-06 14:59:31 +0300

    Add project files.

---
+++ MyAspNetCore.Web/Controllers/OrnekController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCore.Web.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyAspNetCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAspNetCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
